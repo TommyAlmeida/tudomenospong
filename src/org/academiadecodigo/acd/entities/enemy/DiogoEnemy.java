e6f4c6b9f3e1875c244e8e2655caae2e61ccb48d
diogo.saraiva <diogo.saraiva@hotmail.com>
2018-02-12 10:19:17 +0000

    changed enemies value

--- src/org/academiadecodigo/acd/entities/enemy/DiogoEnemy.java
+++ src/org/academiadecodigo/acd/entities/enemy/DiogoEnemy.java
@@ -3,7 +3,7 @@ package org.academiadecodigo.acd.entities.enemy;
 public class DiogoEnemy extends Enemy {
 
     public DiogoEnemy() {
-        super(EnemyType.DIOGO, 20, 10, 1);
+        super(EnemyType.DIOGO, 20, 10, 5);
     }
 
 }

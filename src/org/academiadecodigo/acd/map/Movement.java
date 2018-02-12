8aad6f90f37d45448d79d1c4d77a9eed54f01cfa
diogo <diogo.saraiva@hotmail.com>
2018-02-12 21:52:38 +0000

    started Movement class

---
+++ src/org/academiadecodigo/acd/map/Movement.java
@@ -0,0 +1,11 @@
+package org.academiadecodigo.acd.map;
+
+public class Movement {
+
+    Position[] positions;
+
+    public void move(Position[] positions, Position){
+        this.positions = positions;
+    }
+
+}
